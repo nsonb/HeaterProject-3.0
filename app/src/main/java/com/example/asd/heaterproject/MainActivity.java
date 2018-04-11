d9c59b66a26732eeefc6805ee0bbf4a0b70d4e38
Salla Vesterinen <salla.vesterinen@gmail.com>
2018-04-11 14:58:27 +0300

    hello

--- app/src/main/java/com/example/asd/heaterproject/MainActivity.java
+++ app/src/main/java/com/example/asd/heaterproject/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //this is the project for Heater
         //Tested with SDK Version API 27
+        //hello oreo not oreo
     }
 }

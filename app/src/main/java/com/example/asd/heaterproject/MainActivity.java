19af2dcdb261247df911439980da717aba6991ff
Salla Vesterinen <salla.vesterinen@gmail.com>
2018-04-14 16:30:36 +0300

    Application created

--- app/src/main/java/com/example/asd/heaterproject/MainActivity.java
+++ app/src/main/java/com/example/asd/heaterproject/MainActivity.java
@@ -1,16 +1,195 @@
+/* Request 20 latest position measurements and displays them on the
+        * listview wigdet.
+        *
+        * Note: you need to insert the following line before application -tag in
+        * the AndroidManifest.xml file
+        *  <uses-permission android:name="android.permission.INTERNET" />
+        *
+        * Author(s): Jarkko Vuori
+        * Modification(s):
+        *   First version created on 04.02.2017
+        *   Clears the positions array before button pressed 15.02.2017
+        *   Stores username and password to SharedPreferences 17.02.2017
+        */
 package com.example.asd.heaterproject;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int    MAXPOSITIONS = 20;
+    private static final String PREFERENCEID = "Credentials";
+
+    private String               username, password;
+    private String[]             positions = new String[MAXPOSITIONS];
+    private ArrayAdapter<String> myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this is the project for Heater
-        //Tested with SDK Version API 27
-        //hello oreo
+
+        // initialize the array so that every position has an object (even it is empty string)
+        for (int i = 0; i < positions.length; i++)
+            positions[i] = "";
+
+        // setup the adapter for the array
+        myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, positions);
+
+        // then connect it to the list in application's layout
+        ListView listView = (ListView) findViewById(R.id.mylist);
+        listView.setAdapter(myAdapter);
+
+        // setup the button event listener to receive onClick events
+        ((Button)findViewById(R.id.mybutton)).setOnClickListener(this);
+
+        // check that we know username and password for the Thingsee cloud
+        SharedPreferences prefGet = getSharedPreferences(PREFERENCEID, Activity.MODE_PRIVATE);
+        username = prefGet.getString("username", "");
+        password = prefGet.getString("password", "");
+        if (username.length() == 0 || password.length() == 0)
+            // no, ask them from the user
+            queryDialog(this, getResources().getString(R.string.prompt));
+    }
+
+    private void queryDialog(Context context, String msg) {
+        // get prompts.xml view
+        LayoutInflater li = LayoutInflater.from(context);
+        View promptsView = li.inflate(R.layout.credentials_dialog, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final TextView dialogMsg      = (TextView) promptsView.findViewById(R.id.textViewDialogMsg);
+        final EditText dialogUsername = (EditText) promptsView.findViewById(R.id.editTextDialogUsername);
+        final EditText dialogPassword = (EditText) promptsView.findViewById(R.id.editTextDialogPassword);
+
+        dialogMsg.setText(msg);
+        dialogUsername.setText(username);
+        dialogPassword.setText(password);
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // get user input and set it to result
+                                username = dialogUsername.getText().toString();
+                                password = dialogPassword.getText().toString();
+
+                                SharedPreferences prefPut = getSharedPreferences(PREFERENCEID, Activity.MODE_PRIVATE);
+                                SharedPreferences.Editor prefEditor = prefPut.edit();
+                                prefEditor.putString("username", username);
+                                prefEditor.putString("password", password);
+                                prefEditor.commit();
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
+    public void onClick(View v) {
+        Log.d("USR", "Button pressed");
+
+        // we make the request to the Thingsee cloud server in backgroud
+        // (AsyncTask) so that we don't block the UI (to prevent ANR state, Android Not Responding)
+        new TalkToThingsee().execute("QueryState");
+    }
+
+    /* This class communicates with the ThingSee client on a separate thread (background processing)
+     * so that it does not slow down the user interface (UI)
+     */
+    private class TalkToThingsee extends AsyncTask<String, Integer, String> {
+        ThingSee       thingsee;
+        List<Location> coordinates = new ArrayList<Location>();
+
+        @Override
+        protected String doInBackground(String... params) {
+            String result = "NOT OK";
+
+            // here we make the request to the cloud server for MAXPOSITION number of coordinates
+            try {
+                thingsee = new ThingSee(username, password);
+
+                JSONArray events = thingsee.Events(thingsee.Devices(), MAXPOSITIONS);
+                //System.out.println(events);
+                coordinates = thingsee.getPath(events);
+
+//                for (Location coordinate: coordinates)
+//                    System.out.println(coordinate);
+                result = "OK";
+            } catch(Exception e) {
+                Log.d("NET", "Communication error: " + e.getMessage());
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // check that the background communication with the client was succesfull
+            if (result.equals("OK")) {
+                // now the coordinates variable has those coordinates
+                // elements of these coordinates is the Location object who has
+                // fields for longitude, latitude and time when the position was fixed
+                for (int i = 0; i < coordinates.size(); i++) {
+                    Location loc = coordinates.get(i);
+
+                    positions[i] = (new Date(loc.getTime())) +
+                            " (" + loc.getLatitude() + "," +
+                            loc.getLongitude() + ")"; //coordinates.get(i).toString();
+                }
+            } else {
+                // no, tell that to the user and ask a new username/password pair
+                positions[0] = getResources().getString(R.string.no_connection);
+                queryDialog(MainActivity.this, getResources().getString(R.string.info_prompt));
+            }
+            myAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // first clear the previous entries (if they exist)
+            for (int i = 0; i < positions.length; i++)
+                positions[i] = "";
+            myAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {}
     }
 }
---
+++ app/src/main/java/com/example/asd/heaterproject/ThingSee.java
@@ -0,0 +1,273 @@
+/*
+ * ThingSee.java -- ThingSee Cloud server client
+ *
+ * API documentation:
+ *  http://api.thingsee.com/doc/rest
+ *  https://thingsee.zendesk.com/hc/en-us/articles/205188962-Thingsee-Property-API-
+ *  https://thingsee.zendesk.com/hc/en-us/articles/205188982-Thingsee-Events-API-
+ * Web interface:
+ *  http://app.thingsee.com
+ *
+ * Copyright (C) 2017 by ZyMIX Oy. All rights reserved.
+ * Author(s): Jarkko Vuori
+ * Modification(s):
+ *   First version created on 04.02.2017
+ */
+package com.example.asd.heaterproject;
+
+import android.location.Location;
+import android.util.Log;
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.json.*;
+
+public class ThingSee {
+    private final static String charset = "UTF-8";
+    private final static String url     = "http://api.thingsee.com/v2";
+
+    private URLConnection connection;
+    private String        accountAuthUuid;
+    private String        accountAuthToken;
+    private Boolean       fConnection;
+
+    /**
+     * Authenticates the user
+     * <p>
+     * Credentials returned from the cloud server are used in the following method calls
+     * (so that it is not needed to send email/password information every time to the server).
+     *
+     * @param email      User's email address used for the authentication
+     * @param passwd     User's password
+     * @throws Exception Gives an exception with text information if there was an error
+     */
+    ThingSee(String email, String passwd) throws Exception {
+        JSONObject param = new JSONObject();
+
+        param.put("email", email);
+        param.put("password", passwd);
+
+        JSONObject resp = getThingSeeObject(param, "/accounts/login");
+        accountAuthUuid = resp.getString("accountAuthUuid");
+        accountAuthToken = resp.getString("accountAuthToken");
+    }
+
+    /**
+     * Send a request to the ThingSee server at the subpath
+     * <p>
+     * Authentication is supposed to have been done before (using a constructor)
+     *
+     * @param  request   Request parameter (optional, null if not needed)
+     * @param  path      URI-name for the object to be requested
+     * @return           Requested object in JSON-format
+     * @throws Exception Gives an exception with text information if there was an error
+     */
+    private JSONObject getThingSeeObject(JSONObject request, String path) throws Exception {
+        JSONObject     resp     = null;
+        InputStream    response = null;
+        BufferedReader reader   = null;
+
+
+        fConnection = false;
+        try {
+            connection = new URL(url + path).openConnection();
+            connection.setRequestProperty("Accept-Charset", charset);
+            connection.setRequestProperty("Content-Type", "application/json;charset=" + charset);
+            if (accountAuthToken != null)
+                connection.setRequestProperty("Authorization", "Bearer " + accountAuthToken);
+
+            // send a request (if needed)
+            if (request != null) {
+                connection.setDoOutput(true);   // Triggers HTTP POST request
+                OutputStream output = connection.getOutputStream();
+                output.write(request.toString().getBytes(charset));
+            }
+
+            // wait for the reply
+            response = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(response));
+            StringBuilder out = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                out.append(line);
+            }
+
+            //System.out.println("Responce: " + out.toString());
+            resp = new JSONObject(out.toString());
+            fConnection = true;
+        } finally {
+            // ensure that streams are closed in all situations
+            try {
+                if (response != null)
+                    response.close();
+                if (reader != null)
+                    reader.close();
+            } catch (IOException ioe) {}
+        }
+
+        return (resp);
+    }
+
+    /**
+     * Request the handle to the first ThingSee device at the cloud account
+     * <p>
+     * Cloud account may have multiple devices, this function selects always the first device
+     *
+     * @return Return JSON decription of the selected device
+     */
+    public JSONObject Devices() {
+        JSONObject item, resp;
+
+        try {
+            resp = getThingSeeObject(null, "/devices");
+            JSONArray devices = (JSONArray) resp.get("devices");
+            item = devices.getJSONObject(0);
+        } catch (Exception e) {
+            Log.d("THINGSEE", "No Thingsee device");
+            item = null;
+        }
+
+        return (item);
+    }
+
+    /**
+     * Request all device events
+     * <p>
+     * Every device has an event log. This method read the given devices event log.
+     *
+     * @param  device    Device JSON description (given by Devices() method
+     * @param  limit     Maximum number of events retrieved
+     * @return           Events in JSON format
+     * @throws Exception Gives an exception with text information if there was an error
+     */
+    public JSONArray Events(JSONObject device, int limit) throws Exception {
+        JSONObject resp;
+        JSONArray  events;
+
+        try {
+            resp = getThingSeeObject(null, "/events/" + device.getString("uuid") + "?limit=" + limit);
+            events  = (JSONArray)resp.get("events");
+        } catch (Exception e) {
+            Log.d("THINGSEE", "ThingseeEvents error " + e);
+            throw new Exception("No events");
+        }
+
+        return (events);
+    }
+
+    /* senseID groupID field */
+    private static final int GROUP_LOCATION     = 0x01 << 16;
+    private static final int GROUP_SPEED        = 0x02 << 16;
+    private static final int GROUP_ENERGY       = 0x03 << 16;
+    private static final int GROUP_ORIENTATION  = 0x04 << 16;
+    private static final int GROUP_ACCELERATION = 0x05 << 16;
+    private static final int GROUP_ENVIRONMENT  = 0x06 << 16;
+    private static final int GROUP_HW_KEYS      = 0x07 << 16;
+
+    /* senseID propertyID field */
+    private static final int PROPERTY1          = 0x01 << 8;
+    private static final int PROPERTY2          = 0x02 << 8;
+    private static final int PROPERTY3          = 0x03 << 8;
+    private static final int PROPERTY4          = 0x04 << 8;
+
+    /**
+     * Obtain Location objects from the events array
+     * <p>
+     * Collects all location events and construct Location object
+     *
+     * @param  events Device JSON description (given by Devices() method)
+     * @return        List of Location objects (coordinates), empty if there are no coordinates available
+     * @throws        Exception Gives an exception with text information if there was an error
+     */
+    public List getPath(JSONArray events) throws Exception {
+        List   coordinates = new ArrayList();
+        int    k;
+
+        try {
+            for (int i = 0; i < events.length(); i++) {
+                JSONObject event = events.getJSONObject(i);
+                Location   loc   = new Location("=='");
+
+                loc.setTime(event.getLong("timestamp"));
+                k = 0;
+                JSONArray senses = event.getJSONObject("cause").getJSONArray("senses");
+                for (int j = 0; j < senses.length(); j++) {
+                    JSONObject sense   = senses.getJSONObject(j);
+                    int        senseID = Integer.decode(sense.getString("sId"));
+                    double     value   = sense.getDouble("val");
+
+
+                    switch (senseID) {
+                        case GROUP_LOCATION | PROPERTY1:
+                            loc.setLatitude(value);
+                            k++;
+                            break;
+
+                        case GROUP_LOCATION | PROPERTY2:
+                            loc.setLongitude(value);
+                            k++;
+                            break;
+                    }
+
+                    if (k == 2) {
+                        coordinates.add(loc);
+                        k = 0;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new Exception("No coordinates");
+        }
+
+        return coordinates;
+    }
+
+    @Override
+    public String toString() {
+        String s;
+
+        if (fConnection)
+            s = "Uuid: " + accountAuthUuid + "\nToken: " + accountAuthToken;
+        else
+            s = "Not authenticated";
+
+        return (s);
+    }
+
+    /**
+     * Convert events to string
+     * <p>
+     * Converts timestamp and senses information of the event to string
+     *
+     * @param  events Events in JSON format
+     * @return        Events in string format
+     */
+    public String toString(JSONArray events) {
+        StringBuilder s = new StringBuilder();
+        String        ss;
+
+        try {
+            for (int i = 0; i < events.length(); i++) {
+                JSONObject event = events.getJSONObject(i);
+
+                s.append(new Date(event.getLong("timestamp")) + ": ");
+                //System.out.println("    type: " + event.getString("type"));
+                JSONArray senses = event.getJSONObject("cause").getJSONArray("senses");
+                for (int j = 0; j < senses.length(); j++) {
+                    JSONObject sense = senses.getJSONObject(j);
+
+                    s.append("sId " + sense.getString("sId") + ": " + sense.getDouble("val") + ",");
+                }
+                s.append("\n");
+            }
+            ss = s.toString();
+        } catch (Exception e) {
+            ss = null;
+        }
+
+        return ss;
+    }
+}

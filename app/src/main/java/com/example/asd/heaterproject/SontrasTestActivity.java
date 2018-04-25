be5d97111981a3d7bcaf61266650eb7b014debd0
surowka <sontra.samela@gmail.com>
2018-04-25 14:24:25 +0300

    adds a working Google Maps activity

--- app/src/main/java/com/example/asd/heaterproject/SontrasTestActivity.java
+++ app/src/main/java/com/example/asd/heaterproject/SontrasTestActivity.java
@@ -17,13 +17,25 @@ public class SontrasTestActivity extends AppCompatActivity implements View.OnCli
         // find text view
         TextView intro = (TextView) findViewById(R.id.textView1);
 
-        // find button
+        // find buttons
         Button goBack = (Button) findViewById(R.id.goBackButton);
+        Button openMap = (Button) findViewById(R.id.openMapButton);
         goBack.setOnClickListener(this);
+        openMap.setOnClickListener(this);
+
+        intro.setText("Now with a Maps activity!");
     }
 
     @Override
     public void onClick(View v) {
-        finish();
+        switch (v.getId()){
+            case R.id.openMapButton:
+                Intent maps = new Intent(this, MapsActivity.class);
+                startActivity(maps);
+                break;
+            case R.id.goBackButton:
+                finish();
+                break;
+        }
     }
 }
